6005eaed6628b01c7acab7cd549557c0dd62e71d
Gaetangou <gaetan.goussard@centrale.centralelille.fr>
2021-02-08 21:59:11 +0100

    j'ai rajouté le script de déplacement

---
+++ sceneGenerale/Assets/Scripts/Deplacement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Deplacement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+   
+    // Start is called before the first frame update
+    void Start() {
+
+        agent = GetComponent<NavMeshAgent> ();
+        
+    }
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hit))
+            {
+                Vector3 newTargetPos = hit.point;
+                agent.SetDestination(newTargetPos);
+            }
+        }
+        if (Input.touchCount > 0)
+        {
+            RaycastHit hit2;
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(ray, out hit2))
+            {
+                Vector3 newTargetPos = hit2.point;
+                agent.SetDestination(newTargetPos);
+            }
+        }
+
+
+    }
+}
