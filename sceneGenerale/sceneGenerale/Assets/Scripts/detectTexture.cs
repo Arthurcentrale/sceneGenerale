8e3ce4d627cd435baa099ec8641a3592db06d093
nbethune <75496992+nbethune@users.noreply.github.com>
2021-03-22 15:18:32 +0100

    tests divers

---
+++ sceneGenerale/sceneGenerale/Assets/Scripts/detectTexture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assemblies.Terrain;
+
+
+
+public class detectTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void detection()
+    {
+        Debug.Log(this.GetComponent(normalmapTexture));
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
