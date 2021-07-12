91c0bf1fe36d5f4c2ada4e27d709878c8837f508
Thomas <thomas.beule@centrale.centralelille.fr>
2021-07-12 14:34:48 +0200

    creation architecture habitants

---
+++ sceneGenerale/sceneGenerale/Assets/Scripts/Habitants/HabitantBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HabitantBehaviour : MonoBehaviour
+{
+    //variables générales :
+    public Vector3 mairiePosition = new Vector3(0, 0, 0); //donne l'endroit où les habitants se rendent quand ils sont recrutables et qu'ils ne sont pas encore logés
+
+    public string nom;
+
+    //image
+    public Sprite image;
+
+    //besoins et déchets
+    public float food_qty;
+    public float food_vrty;
+    public float waste;
+
+    //niveau d'écosensibilisation
+    public float ecopoints;
+
+    //statut dans l'aventure
+    public bool isVillager = false;
+    public bool readyToWork = false;
+
+    //conditions = bâtiments nécessaires pour qu'il puisse travailler et autres
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goToMairie();
+        
+    }
+
+    //fonction dépalçant un personnage disponible mais pas prêt à travailler près de la mairie
+    void goToMairie()
+    {
+        if (isVillager && !readyToWork)
+        {
+            transform.position = mairiePosition;
+        }
+    }
+    
+}
---
+++ sceneGenerale/sceneGenerale/Assets/Scripts/Habitants/HabitantManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HabitantManager : MonoBehaviour
+{
+    //liste de tous les habitants du jeu
+    public GameObject[] habitants;
+
+    public List<GameObject> hierarchyHabitants;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //fonction qu'on appelle à chaque fois qu'il y a un nouvel habitant qui rejoint la communauté (et dont la variable isVillager vaut true)
+    void modifyHierarchy(GameObject newHabitant)
+    {
+        hierarchyHabitants.Add(newHabitant);
+    }
+}
+
+
