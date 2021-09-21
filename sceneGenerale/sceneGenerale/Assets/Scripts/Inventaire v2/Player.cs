4bef9090ed7c2bcb874c9fd45e773e83468d885c
Louis <louis.lions@hotmail.fr>
2021-09-21 16:49:07 +0200

    re-test changements inventaire

--- sceneGenerale/sceneGenerale/Assets/Scripts/Inventaire v2/Player.cs
+++ sceneGenerale/sceneGenerale/Assets/Scripts/Inventaire v2/Player.cs
@@ -11,22 +11,7 @@ public class Player : MonoBehaviour
     {
         //inventory = Inventory.CreateInstance(new List<ItemAmount>());
         inventory = new Inventory(new List<ItemAmount>(), new List<ItemAmount>(),this);
-        uiInventory = GameObject.Find("Inventaire2").transform.GetChild(3).gameObject.GetComponent<UI_Inventory>();
+        uiInventory = GameObject.Find("Inventaire2").transform.GetChild(0).gameObject.GetComponent<UI_Inventory>();
         uiInventory.SetInventory(inventory);
-
-        //ItemWorld.SpawnItemWorld(new Vector3(70, 1.3f, 13), new ItemAmount(Item: Item.Create_Instance("Wood"), Amount: 1));
-        //ItemWorld.SpawnItemWorld(new Vector3(75, 1.3f, 13), new ItemAmount(Item: Item.Create_Instance("Berry"), Amount: 1));
-    }
-
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        Debug.Log("test");
-        ItemWorld itemWorld = collider.GetComponent<ItemWorld>();
-        if (itemWorld != null)
-        {
-            //Touching Item
-            inventory.AddItem(itemWorld.GetItem());
-            itemWorld.DestroySelf();
-        }
     }
 }
