891af9a8e68dffed062dd6fae13dae565474457a
Arthur <arthur.frutchuts@gmail.com>
2021-10-18 23:51:18 +0200

    MenuQuiBugPasMiamMiam

---
+++ sceneGenerale/sceneGenerale/Assets/MenuPrincipal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BoutonJouer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //voir t=9min pour plus d'infos https://www.youtube.com/watch?v=zc8ac_qUXQY&t=590s&ab_channel=Brackeys
+    }
+
+    public void BoutonQuitter()
+    {
+         
+        Application.Quit();
+    }
+}
