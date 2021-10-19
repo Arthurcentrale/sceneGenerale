4e5690b7d7e872745cc639ec26cfd94108eacbcb
Tanguy <tanguy.maury@centrale.centralelille.fr>
2021-10-19 09:46:49 +0200

    ajout prefab panel

---
+++ sceneGenerale/sceneGenerale/Assets/PrefabsProg/Menu/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EntreePanel()
+    {
+        Deplacement.enMenu = true;
+    }
+
+    public void SortiePanel()
+    {
+        Deplacement.enMenu = false;
+    }
+
+    public void Bouton1()
+    {
+
+    }
+
+    public void Bouton2()
+    {
+
+    }
+
+    public void Bouton3()
+    {
+
+    }
+}
