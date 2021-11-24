988eeedc78f20a9c80f26480a0ebb24ea39d8002
Thomas <thomas.beule@centrale.centralelille.fr>
2021-11-24 18:06:25 +0100

    IU retravail step 1

---
+++ sceneGenerale/sceneGenerale/Assets/Scripts/IU generale/livre.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class livre : MonoBehaviour
+{
+    private Animator animatorLivreFerme;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorLivreFerme = GameObject.Find("LivreFerme").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void ouvertureLivre()
+    {
+        animatorLivreFerme.SetTrigger("Selected");
+    }
+}
