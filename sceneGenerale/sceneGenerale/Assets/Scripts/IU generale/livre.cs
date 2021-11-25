fd0f03f8fa84c7dc69877cdaa5a079589ff17807
Thomas <thomas.beule@centrale.centralelille.fr>
2021-11-25 15:22:39 +0100

    IU livre et menu constru finalisation

--- sceneGenerale/sceneGenerale/Assets/Scripts/IU generale/livre.cs
+++ sceneGenerale/sceneGenerale/Assets/Scripts/IU generale/livre.cs
@@ -6,11 +6,18 @@ public class livre : MonoBehaviour
 {
     private Animator animatorLivreFerme;
     private Animator animatorLivreOuvert;
+    private GameObject boutonConstruction;
+    private GameObject menuConstruPageDroite;
+    private GameObject fermetureBouton;
+
     // Start is called before the first frame update
     void Start()
     {
         animatorLivreFerme = GameObject.Find("LivreFerme").GetComponent<Animator>();
         animatorLivreOuvert = GameObject.Find("LivreOuvert").GetComponent<Animator>();
+        boutonConstruction = GameObject.Find("ConstructionBouton");
+        menuConstruPageDroite = GameObject.Find("menuConstructionPageDroite");
+        fermetureBouton = GameObject.Find("FermetureBouton");
     }
 
     // Update is called once per frame
@@ -18,5 +25,27 @@ public class livre : MonoBehaviour
     {
         animatorLivreFerme.SetTrigger("Selected");
         animatorLivreOuvert.SetTrigger("Selected");
+        boutonConstruction.SetActive(true);
+        menuConstruPageDroite.SetActive(false);
+        fermetureBouton.SetActive(false);
+    }
+
+    public void ouvertureMenuConstru()
+    {
+        animatorLivreOuvert.SetTrigger("OuvertureComplete");
+        boutonConstruction.SetActive(false);
+        fermetureBouton.SetActive(true);
+    }
+
+    public void affichagePageDroiteMenuConstru()
+    {
+        menuConstruPageDroite.SetActive(true);
+    }
+
+    public void fermetureLivre()
+    {
+        animatorLivreOuvert.SetTrigger("FermetureComplete");
+        fermetureBouton.SetActive(false);
+        animatorLivreFerme.SetTrigger("Return");
     }
 }
