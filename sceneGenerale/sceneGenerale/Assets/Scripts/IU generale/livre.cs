e68f893d7630fd7f6e22d8fe91a8abe08f900a7e
Thomas <thomas.beule@centrale.centralelille.fr>
2021-11-25 13:41:29 +0100

    IU step 2

--- sceneGenerale/sceneGenerale/Assets/Scripts/IU generale/livre.cs
+++ sceneGenerale/sceneGenerale/Assets/Scripts/IU generale/livre.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class livre : MonoBehaviour
 {
     private Animator animatorLivreFerme;
+    private Animator animatorLivreOuvert;
     // Start is called before the first frame update
     void Start()
     {
         animatorLivreFerme = GameObject.Find("LivreFerme").GetComponent<Animator>();
+        animatorLivreOuvert = GameObject.Find("LivreOuvert").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     public void ouvertureLivre()
     {
         animatorLivreFerme.SetTrigger("Selected");
+        animatorLivreOuvert.SetTrigger("Selected");
     }
 }
