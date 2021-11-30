da02c2d0d570f67c343dbaf4c425c0a2ebe39386
Gaetan <gaetan.goussard@centrale.centralelille.fr>
2021-11-30 17:36:16 +0100

    habitant et recolte ok

--- sceneGenerale/sceneGenerale/Assets/Scripts/Chaumiere.cs
+++ sceneGenerale/sceneGenerale/Assets/Scripts/Chaumiere.cs
@@ -7,14 +7,12 @@ public class Chaumiere : MonoBehaviour
 {
 
     GameObject cible; // On cible la chaumière dont on veut afficher les paramètres
-    public GameObject habitantActuel;
     List<GameObject> habitantsSansMaison;
     public GameObject panel;
-    bool open;
-    bool onPanel;
+    public bool open;
+    public bool onPanel;
     Vector2 mP;
     new public Camera camera;
-    public bool isEmpty;
     private Animator animator;
     GameObject listehabitants;
     public GameObject panelmenu;
@@ -30,7 +28,6 @@ public class Chaumiere : MonoBehaviour
     {
         /*listehabitants = GameObject.Find("listehabitants");
         Debug.Log(listehabitants.transform.childCount);*/
-        isEmpty = true;
         onPanel = false;
         open = false;
         animator = panel.transform.GetChild(0).GetComponent<Animator>();
@@ -56,11 +53,13 @@ public class Chaumiere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var ray = camera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit Hit;
-
         if (Input.GetMouseButtonDown(0))
         {
+            if (open && onPanel)
+            {
+            }
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
             mP = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             if (onPanel == false)
             {
@@ -68,15 +67,17 @@ public class Chaumiere : MonoBehaviour
                 open = false;
             }
 
-            if (open == false && (((Input.mousePosition.x - Screen.width / 2) / (Screen.width / 4) * (Input.mousePosition.x - Screen.width / 2) / (Screen.width / 4)) + ((Input.mousePosition.y - Screen.height / 2) / (Screen.height / 4) * (Input.mousePosition.y - Screen.height / 2) / (Screen.height / 4)) < 1))
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.CompareTag("Chaumière"))
             {
-                if (Physics.Raycast(ray, out Hit) && Hit.collider.CompareTag("Chaumière"))
-                {
+                Debug.Log("salut");
+                if (open == false && (((Input.mousePosition.x - Screen.width / 2) / (Screen.width / 4) * (Input.mousePosition.x - Screen.width / 2) / (Screen.width / 4)) + ((Input.mousePosition.y - Screen.height / 2) / (Screen.height / 4) * (Input.mousePosition.y - Screen.height / 2) / (Screen.height / 4)) < 1))
+                    {
                     cible = Hit.transform.gameObject;
                     //habitantsSansMaison = TrouverHabitantSansMaison();
                     panel.transform.position = new Vector2(mP.x + panel.GetComponent<RectTransform>().rect.width, mP.y);
-                    if (isEmpty)
+                    if (cible.GetComponent<HabitantChaumiere>().isEmpty)
                     {
+                        panel.transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
                         panel.gameObject.SetActive(true);
                         animator.SetTrigger("ouverture1BulleCouper");
                         open = true;
@@ -99,11 +100,13 @@ public class Chaumiere : MonoBehaviour
     public void ClickOnPanel()
     {
         onPanel = true;
+        Debug.Log("in");
     }
 
     public void ClickOutPanel()
     {
         onPanel = false;
+        Debug.Log("out");
     }
     List<GameObject> TrouverHabitantSansMaison()
     {
@@ -117,6 +120,7 @@ public class Chaumiere : MonoBehaviour
                 ha.Add(child.gameObject);
             }
         }
+        Debug.Log("hello" + ha.Count);
         return ha;
     }
 
@@ -127,13 +131,14 @@ public class Chaumiere : MonoBehaviour
         {
             Debug.Log("Je choisi l'habitant 1");
             liste[0].gameObject.GetComponent<HabitantBehaviour>().isHoused = true;
-            habitantActuel = liste[0].gameObject;
+            cible.GetComponent<HabitantChaumiere>().habitantActuel = liste[0].gameObject;
             panelmenu.SetActive(false);
             panel.transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-            panel.gameObject.SetActive(true);
+            panel.gameObject.SetActive(false);
+            open = false;
             animator.SetTrigger("ouverture1BulleCouper");
-            isEmpty = false;
-            CompteurBouffe.Data.NbrBouffe -= habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
+            cible.GetComponent<HabitantChaumiere>().isEmpty = false;
+            CompteurBouffe.Data.NbrBouffe -= cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
             compteurbouffe.CBouffe.text = CompteurBouffe.Data.NbrBouffe.ToString();
             Debug.Log(compteurbouffe.CBouffe.text);
         }
@@ -145,13 +150,14 @@ public class Chaumiere : MonoBehaviour
         {
             Debug.Log("Je choisi l'habitant 2");
             liste[1].gameObject.GetComponent<HabitantBehaviour>().isHoused = true;
-            habitantActuel = liste[1].gameObject;
+            cible.GetComponent<HabitantChaumiere>().habitantActuel = liste[1].gameObject;
             panelmenu.SetActive(false);
             panel.transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-            panel.gameObject.SetActive(true);
+            panel.gameObject.SetActive(false);
+            open = false;
             animator.SetTrigger("ouverture1BulleCouper");
-            isEmpty = false;
-            CompteurBouffe.Data.NbrBouffe -= habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
+            cible.GetComponent<HabitantChaumiere>().isEmpty = false;
+            CompteurBouffe.Data.NbrBouffe -= cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
             compteurbouffe.CBouffe.text = CompteurBouffe.Data.NbrBouffe.ToString();
             Debug.Log(compteurbouffe.CBouffe.text);
         }
@@ -163,13 +169,14 @@ public class Chaumiere : MonoBehaviour
         {
             Debug.Log("Je choisi l'habitant 3");
             liste[2].gameObject.GetComponent<HabitantBehaviour>().isHoused = true;
-            habitantActuel = liste[2].gameObject;
+            cible.GetComponent<HabitantChaumiere>().habitantActuel = liste[2].gameObject;
             panelmenu.SetActive(false);
             panel.transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-            panel.gameObject.SetActive(true);
+            panel.gameObject.SetActive(false);
+            open = false;
             animator.SetTrigger("ouverture1BulleCouper");
-            isEmpty = false;
-            CompteurBouffe.Data.NbrBouffe -= habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
+            cible.GetComponent<HabitantChaumiere>().isEmpty = false;
+            CompteurBouffe.Data.NbrBouffe -= cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
             compteurbouffe.CBouffe.text = CompteurBouffe.Data.NbrBouffe.ToString();
             Debug.Log(compteurbouffe.CBouffe.text);
         }
@@ -181,13 +188,14 @@ public class Chaumiere : MonoBehaviour
         {
             Debug.Log("Je choisi l'habitant 4");
             liste[3].gameObject.GetComponent<HabitantBehaviour>().isHoused = true;
-            habitantActuel = liste[3].gameObject;
+            cible.GetComponent<HabitantChaumiere>().habitantActuel = liste[3].gameObject;
             panelmenu.SetActive(false);
             panel.transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-            panel.gameObject.SetActive(true);
+            panel.gameObject.SetActive(false);
+            open = false;
             animator.SetTrigger("ouverture1BulleCouper");
-            isEmpty = false;
-            CompteurBouffe.Data.NbrBouffe -= habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
+            cible.GetComponent<HabitantChaumiere>().isEmpty = false;
+            CompteurBouffe.Data.NbrBouffe -= cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().foodQuantity;
             compteurbouffe.CBouffe.text = CompteurBouffe.Data.NbrBouffe.ToString();
             Debug.Log(compteurbouffe.CBouffe.text);
         }
@@ -197,6 +205,7 @@ public class Chaumiere : MonoBehaviour
     {
         List<GameObject> liste = TrouverHabitantSansMaison();
         int i = liste.Count;
+        Debug.Log("ccccc" + i);
         int h= 0;
         foreach (Transform child in panelmenu.transform.GetChild(0))
         {
@@ -215,7 +224,7 @@ public class Chaumiere : MonoBehaviour
 
     void FctInfo()
     {
-        if (habitantActuel == null)
+        if (cible.GetComponent<HabitantChaumiere>().habitantActuel == null)
         {
             menuinfo.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Aucun Habitant";
             menuinfo.transform.GetChild(4).gameObject.SetActive(false);
@@ -227,14 +236,14 @@ public class Chaumiere : MonoBehaviour
         }
         else
         {
-            menuinfo.transform.GetChild(2).gameObject.GetComponent<Text>().text = habitantActuel.GetComponent<HabitantBehaviour>().nom;
+            menuinfo.transform.GetChild(2).gameObject.GetComponent<Text>().text = cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().nom;
             for(int i =0;i<3; i++)
             {
-                if (i > habitantActuel.GetComponent<HabitantBehaviour>().foodVariety -1 )
+                if (i > cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().foodVariety -1 )
                     menuinfo.transform.GetChild(4 + i).gameObject.SetActive(false);
                 else menuinfo.transform.GetChild(4 + i).gameObject.SetActive(true); 
             }
-            menuinfo.transform.GetChild(8).gameObject.GetComponent<Image>().sprite = habitantActuel.GetComponent<HabitantBehaviour>().image ;
+            menuinfo.transform.GetChild(8).gameObject.GetComponent<Image>().sprite = cible.GetComponent<HabitantChaumiere>().habitantActuel.GetComponent<HabitantBehaviour>().image ;
             menuinfo.transform.GetChild(8).gameObject.SetActive(true);
             panel.SetActive(false);
             menuinfo.SetActive(true);
