1f0644e1d24a6e2572491a6460516c2294e95625
Thomas <thomas.beule@centrale.centralelille.fr>
2021-12-02 10:07:50 +0100

    iu bat step 2

--- sceneGenerale/sceneGenerale/Assets/Scripts/Construction/ScriptBâti/IUBatis.cs
+++ sceneGenerale/sceneGenerale/Assets/Scripts/Construction/ScriptBâti/IUBatis.cs
@@ -14,14 +14,14 @@ public class IUBatis : MonoBehaviour
     new public Camera camera;
     private Animator animator;
 
-    float timer = 0f;
-    float delai = 5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         panel = GameObject.Find("PanelBatisConstruction");
+        camera = GameObject.Find("Camera").GetComponent<Camera>();
+        animator = panel.transform.GetChild(0).GetComponent<Animator>();
 
         onPanel = false;
         open = false;
@@ -31,7 +31,34 @@ public class IUBatis : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mP = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            if (onPanel == false)
+            {
+                panel.SetActive(false);
+                open = false;
+            }
+
+            if (open == false && (((Input.mousePosition.x - Screen.width / 2) / (Screen.width / 4) * (Input.mousePosition.x - Screen.width / 2) / (Screen.width / 4)) + ((Input.mousePosition.y - Screen.height / 2) / (Screen.height / 4) * (Input.mousePosition.y - Screen.height / 2) / (Screen.height / 4)) < 1))
+            {
+                if (Physics.Raycast(ray, out Hit) && Hit.collider.CompareTag("BatiChaumière"))
+
+                {
+                    //Ajouter update valeur max du slider, etc..
+                    panel.transform.position = new Vector2(mP.x + panel.GetComponent<RectTransform>().rect.width, mP.y);
+                    panel.gameObject.SetActive(true);
+                    animator.SetTrigger("Ouverture");
+                    open = true;
+
+                }
+
+            }
+
+        }
     }
 
     public void ClickOnPanel()
