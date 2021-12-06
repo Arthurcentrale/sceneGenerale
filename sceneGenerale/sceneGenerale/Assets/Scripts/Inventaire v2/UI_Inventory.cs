e217dcf6c0be73b8cd43036d5123e5e39f7b396f
Louis <louis.lions@hotmail.fr>
2021-12-06 20:42:51 +0100

    Sauvegarde inventaire

--- sceneGenerale/sceneGenerale/Assets/Scripts/Inventaire v2/UI_Inventory.cs
+++ sceneGenerale/sceneGenerale/Assets/Scripts/Inventaire v2/UI_Inventory.cs
@@ -157,10 +157,11 @@ public class UI_Inventory : MonoBehaviour
         inventory = _inventory;
 
         inventory.OnItemListChanged += Inventory_OnItemListChanged;
-        
+
+        inventory.AddItem(new ItemAmount(Item: Hache, Amount: 1));
         /*
         inventory.AddItem(new ItemAmount(Item: Bois, Amount: 2));
-        inventory.AddItem(new ItemAmount(Item: Hache, Amount: 1));
+        
         inventory.AddItem(new ItemAmount(Item: Pioche, Amount: 1));
         inventory.AddItem(new ItemAmount(Item: Pierre, Amount: 2));
         */
