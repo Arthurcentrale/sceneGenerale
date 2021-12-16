aff3a02cb0c480d492b5eb297a00050c1f8ec138
Thomas <thomas.beule@centrale.centralelille.fr>
2021-12-16 17:16:01 +0100

    Auto stash before merge of "onsenfoutsiellepete" and "origin/onsenfoutsiellepete"

---
+++ sceneGenerale/sceneGenerale/Assets/Scripts/IU generale/dialogueOuvrier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogueOuvrier : MonoBehaviour
+{
+
+    private Text texteBoiteDialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        texteBoiteDialogue = GameObject.Find("Boite").transform.GetChild(0).GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void lancementDialogueOuvrier()
+    {
+        texteBoiteDialogue.text = "Salut l'ami ! Moi, c'est l'ouvrier de la communauté. Je serais heureux de t'aider à bâtir toutes sortes de structures pour que le village s'étende.";
+    }
+}
