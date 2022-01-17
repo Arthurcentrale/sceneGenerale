f36d3e0dc7b53ec69c028a048d13acf8306fb599
Thomas <thomas.beule@centrale.centralelille.fr>
2022-01-17 14:29:28 +0100

    tests onbecamevisible

---
+++ sceneGenerale/sceneGenerale/Assets/Scripts/optimisation/testdesacAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testdesacAnim : MonoBehaviour
+{
+    public bool visibilite = true;
+    // Start is called before the first frame update
+    public void OnBecameInvisible()
+    {
+        visibilite = false;
+        //gameObject.SetActive(false);
+        Debug.Log("invisible");
+    }
+
+    public void OnBecameVisible()
+    {
+        visibilite = true;
+        Debug.Log("visible");
+        //gameObject.SetActive(true);
+    }
+}
